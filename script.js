26e5dca7abf9bc61646db63b9fd4c76b3e871061
Eric Nathan <ericnathansouza@gmail.com>
2023-09-05 18:38:18 -0300

    novo projeto

---
+++ script.js
@@ -0,0 +1,26 @@
+function adicionarCaracter(caracter){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value = valorDisplay + caracter
+        
+}
+
+function limpaTela(){
+    document.querySelector(".display").value = ""
+}
+
+function calcular(){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value = eval(valorDisplay)
+}
+
+function inverter(){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value = valorDisplay * -1
+}
+
+
+
+
